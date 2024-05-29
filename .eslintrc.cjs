7cc6e21f205903693c3a8b9679a0755ea7ff7594
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-05-30 01:55:09 +0800

    init commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json', // path of tsconfig file
+  },
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 0,
+    'linebreak-style': ['error', 'unix'], // enforce Unix line endings
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <p className="text-5xl">Hello World</p>
+    </div>
+  );
+}
+
+export default App;
