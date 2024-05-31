ff0892ed1f014b867e429cfbce41db132b9c9cdb
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-05-31 13:04:04 +0800

    Add Device List component

---
+++ src/components/DeviceList/index.tsx
@@ -0,0 +1,67 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './styles.css';
+import { NavLink } from 'react-router-dom';
+import MyButton from '../Button';
+import Card from '../Card';
+
+interface Device {
+  id: string;
+  title: string;
+  type: string;
+  thumb: string;
+  price: number;
+}
+
+interface DeviceListProps {
+  devices: Device[];
+}
+
+function DeviceList({ devices }: DeviceListProps): JSX.Element {
+  const renderSlides = (): JSX.Element[] =>
+    devices.map((device) => (
+      <div key={device.id}>
+        <Card className="flex-col justify-center items-center hover:opacity-90 cursor-pointer">
+          <NavLink to={`/${device.type}/${device.id}`}>
+            <div className="slide-content">
+              <img
+                src={device.thumb}
+                alt={device.title}
+                className="slide-image"
+              />
+              <h3 className="slide-title">{device.title}</h3>
+              <p className="slide-price">${device.price}</p>
+            </div>
+          </NavLink>
+          <MyButton
+            variant="contained"
+            style={{
+              backgroundColor: '#0071E3',
+              color: 'white',
+              borderRadius: '10px',
+              marginTop: '20px',
+            }}
+          >
+            Learn More
+          </MyButton>
+        </Card>
+      </div>
+    ));
+
+  return (
+    <div className="Slider">
+      <Slider
+        dots
+        slidesToShow={3}
+        slidesToScroll={2}
+        autoplay
+        autoplaySpeed={5000}
+      >
+        {renderSlides()}
+      </Slider>
+    </div>
+  );
+}
+
+export default DeviceList;
