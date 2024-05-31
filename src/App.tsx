7d3a4449d3794d80d254e8ddcd3374218c51d8e4
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-05-31 13:03:13 +0800

    Add routes and react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layout';
 import HomePage from './pages/HomePage';
+import IpadPage from './pages/IpadPage';
+import IphonePage from './pages/IphonePage';
+import MacPage from './pages/MacPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path="ipad" element={<IpadPage />} />
+        <Route path="iphone" element={<IphonePage />} />
+        <Route path="mac" element={<MacPage />} />
 
         <Route path="*" element={<NotFoundPage />} />
       </Route>
