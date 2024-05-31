a40d7be64574b3d55543ef76d6a8f3ea4398903a
Bob Kyle L. Rosales <bobkyle.rosales@cit.edu>
2024-06-01 03:33:44 +0800

    Update route

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,31 @@ import HomePage from './pages/HomePage';
 import IpadPage from './pages/IpadPage';
 import IphonePage from './pages/IphonePage';
 import MacPage from './pages/MacPage';
+import DevicePage from './pages/DevicePage';
 import NotFoundPage from './pages/NotFoundPage';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="ipad" element={<IpadPage />} />
-        <Route path="iphone" element={<IphonePage />} />
-        <Route path="mac" element={<MacPage />} />
+        <Route path="ipad">
+          <Route index element={<IpadPage />} />
+          <Route path=":id" element={<DevicePage device="iPad" />} />
+        </Route>
+        <Route path="iphone">
+          <Route index element={<IphonePage />} />
+          <Route path=":id" element={<DevicePage device="iPhone" />} />
+        </Route>
+        <Route path="mac">
+          <Route index element={<MacPage />} />
+          <Route path=":id" element={<DevicePage device="Mac" />} />
+        </Route>
 
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
